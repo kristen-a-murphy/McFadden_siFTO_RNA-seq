40d34b9d6093592cc3cd362d27a1c1c55e4d5353
kristen-a-murphy <78400374+kristen-a-murphy@users.noreply.github.com>
2021-06-28 15:38:49 -0400

    Update top300isgs.py
    Labeling Points on Mock Dataset

--- top300isgs.py
+++ top300isgs.py
@@ -23,29 +23,30 @@ ctrllFC = df.iloc[:,13]
 ctrllpa = df.iloc[:,15]
 bluelFC = []
 bluelpa = []
-for ind in range(len(ifnlpa)):
-    if ifnlpa[ind] > 2:
-        bluelFC.append(ifnlFC[ind])
-        bluelpa.append(ifnlpa[ind])
+for ind in range(len(mocklpa)):
+    if mocklpa[ind] > 2:
+        bluelFC.append(mocklFC[ind])
+        bluelpa.append(mocklpa[ind])
 
-#%% Creating the Figure
-#fig, ax = plt.subplots(num = 1, clear = True)
-#ax.plot(ctrllFC, ctrllpa, "k.")
-#ax.plot(mocklFC, mocklpa, "k.")
-#ax.plot(ifnlFC, ifnlpa, "k.")
-#ax.plot(bluelFC, bluelpa, "b.")
-#ax.set(xlabel = "log$_{2}$ Fold Change", ylabel = "-log$_{10}$ adj P-Value", title = "300 ISGs (siFTO IFN/ siCTRL IFN)")
-#ax.grid(True)
-#fig.tight_layout()
-#fig.savefig("top300_ISGs_volcano_ifn_blue.pdf", dpi = 400)
+#%% Labeling Points
+IFITM1m = (5.465700478, 2.362336796)
+IFIT3m = (1.232024232, 12.20234018)
+ISG15m = (-0.032925825, 0.022809397)
+EIF2AK2m = (-0.054236867, 0.038615387)
+arrow_props = dict(facecolor="black", width=0.5, headwidth=4)
 
 #%% Broken Axes
 fig = plt.figure()
-bax = brokenaxes(ylims= ((-2, 50), (105, 120)))
-bax.plot(ifnlFC, ifnlpa, "k.")
+bax = brokenaxes(ylims= ((-2, 30), (65, 75)))
+bax.plot(mocklFC, mocklpa, "k.")
 bax.plot(bluelFC, bluelpa, "b.")
+bax.annotate("IFITM1", xy=IFITM1m, xytext=(4.5, 8), arrowprops=arrow_props)
+bax.annotate("IFIT3", xy=IFIT3m, xytext=(0, 18), arrowprops=arrow_props)
+bax.annotate("ISG15", xy=ISG15m, xytext=(0, 8), arrowprops=arrow_props)
+bax.annotate("EIF2AK2", xy=EIF2AK2m, xytext=(-1.25, 8), arrowprops=arrow_props)
 bax.set_xlabel('log$_{2}$ Fold Change')
+bax.set_xlim([-2.5, 5.75])
 bax.set_ylabel('-log$_{10}$ adj P-Value')
-bax.set_title('300 ISGs (siFTO IFN/ siCTRL IFN)')
+bax.set_title('300 ISGs (siFTO Mock/ siCTRL Mock)')
 bax.grid(True)
-fig.savefig("top300_ISGs_baxifn.pdf", dpi=400)
+fig.savefig("top300_ISGs_baxmock.pdf", dpi=400)
